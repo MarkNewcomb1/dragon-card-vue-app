683cff9feec953a7b09a833cf3b9aed39bcdd277
MarkNewcomb1 <wlmmn@hotmail.com>
2018-07-24 13:00:35 -0600

    initial commit

---
+++ src/components/Card.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="card">
+    <h2>I'm {{dragon.name}}</h2>
+    <img :src='dragon.imageUrl'>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: ['dragon']
+}
+</script>
+<style scoped>
+.card {
+    width: 25%;
+    border: 1px solid black;
+    background: lightgray;
+}
+.card img {
+    max-width: 100%;
+}
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,36 @@
+<template>
+<div>
+    <Card v-for="dragon in dragons" v-bind:key="dragon.id" v-bind:dragon="dragon" />
+</div>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+    name: "CardList",
+    components: {
+        Card
+    },
+    data() {
+        return {
+            dragons: [
+            {name: 'Tiamat', 
+            id: 1,
+            imageUrl: 'https://gbf.wiki/images/thumb/9/94/Tiamat_A.png/480px-Tiamat_A.png'
+            }, 
+            {name: 'Smaug', 
+            id: 2,
+            imageUrl: 'https://icdn2.digitaltrends.com/image/the-hobbit-the-desolation-of-smaug-fx-046-640x640.jpg?ver=1.jpg'
+            },
+            {name: 'Puff', 
+            id: 3,
+            imageUrl: 'https://assets.pinshape.com/uploads/image/file/111046/container_magic-puff-dragon-3d-printing-111046.jpg'
+            }
+            ]
+        }
+    }
+}
+</script>
+<style>
+</style>
